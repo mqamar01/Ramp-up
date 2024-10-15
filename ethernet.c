ba40c8fe8d35e6ca9c55f41a9bb8ed9bc7b77894
Syed Muhammad Murtaza <syed.murtaza@dreambigsemi.com>
2024-10-15 15:54:08 +0500

    Fixes to ethernet.c & next_proto identifier added

--- ethernet.c
+++ ethernet.c
@@ -9,9 +9,9 @@
 
 typedef struct Header
 {
-    unsigned long int destination : 48 ;
-    unsigned long int source : 48 ;
-    unsigned short int lenght : 16 ;
+    unsigned long int destination;
+    unsigned long int source;
+    unsigned short int lenght;
 }Header;
 
 void parseheader(Header obj)
@@ -25,25 +25,28 @@ void parseheader(Header obj)
 void parseLE(Header obj)
 {
     printf("\nDetails in Little Endian HEX Format\n");
-    __uint8_t Db0 = 0xFF & ((obj.destination)>>40);
-    __uint8_t Db1 = 0xFF & ((obj.destination)>>32);
-    __uint8_t Db2 = 0xFF & ((obj.destination)>>24);
-    __uint8_t Db3 = 0xFF & ((obj.destination)>>16);
-    __uint8_t Db4 = 0xFF & ((obj.destination)>>8);
-    __uint8_t Db5 = 0xFF & ((__uint8_t)obj.destination);
-    printf("Destination MAC: %x%x%x%x%x%x",Db5,Db4,Db3,Db2,Db1,Db0);
+    __uint64_t Db0 = 0x00000000000000FF & (obj.destination>>40);
+    __uint64_t Db1 = 0x000000000000FF00 & (obj.destination>>24);
+    __uint64_t Db2 = 0x0000000000FF0000 & (obj.destination>>8);
+    __uint64_t Db3 = 0x00000000FF000000 & (obj.destination<<8);
+    __uint64_t Db4 = 0x000000FF00000000 & (obj.destination<<24);
+    __uint64_t Db5 = 0x0000FF0000000000 & (obj.destination<<40);
+    __uint64_t socket = Db0 | Db1 | Db2 | Db3 | Db4 | Db5 ;
+    printf("Destination MAC: %lx",socket);
     printf("\n");
-    __uint8_t Sb0 = 0xFF & ((obj.source)>>40);
-    __uint8_t Sb1 = 0xFF & ((obj.source)>>32);
-    __uint8_t Sb2 = 0xFF & ((obj.source)>>24);
-    __uint8_t Sb3 = 0xFF & ((obj.source)>>16);
-    __uint8_t Sb4 = 0xFF & ((obj.source)>>8);
-    __uint8_t Sb5 = 0xFF & ((__uint8_t)obj.source);
-    printf("Source MAC: %x%x%x%x%x%x",Sb5,Sb4,Sb3,Sb2,Sb1,Sb0);
+    __uint64_t Sb0 = 0x00000000000000FF & (obj.source>>40);
+    __uint64_t Sb1 = 0x000000000000FF00 & (obj.source>>24);
+    __uint64_t Sb2 = 0x0000000000FF0000 & (obj.source>>8);
+    __uint64_t Sb3 = 0x00000000FF000000 & (obj.source<<8);
+    __uint64_t Sb4 = 0x000000FF00000000 & (obj.source<<24);
+    __uint64_t Sb5 = 0x0000FF0000000000 & (obj.source<<40);
+    __uint64_t socket1 = Sb0 | Sb1 | Sb2 | Sb3 | Sb4 | Sb5 ;
+    printf("Source MAC: %lx",socket1);
     printf("\n");
-    __uint8_t Lb0 = 0xFF & ((obj.lenght)>>8);
-    __uint8_t Lb1 = 0xFF & ((__uint8_t)obj.lenght);
-    printf("Length: %x%x",Lb1,Lb0);
+    __uint16_t Lb0 = 0x00FF & (obj.lenght>>8);
+    __uint16_t Lb1 = 0xFF00 & (obj.lenght<<8);
+    __uint16_t socket2 = Lb0 | Lb1;
+    printf("Length: %lx",socket2);
     printf("\n");
 }
 
@@ -52,7 +55,7 @@ void typecheck(Header obj)
     __uint8_t mask = 0xFF & ((obj.destination)>>40);
     if (mask == 0xFF)
     {
-        printf("\nAdress is broadcast!\n");
+        printf("\nAdress is broadcast\n");
         return;
     }
     if (mask % 2)
@@ -61,8 +64,22 @@ void typecheck(Header obj)
     }
     if ((mask % 2) == 0)
     {
-        printf("\nAdress is unicast!\n");
+        printf("\nAdress is unicast\n");
     }
+}
+
+void next_proto(Header obj)
+{
+    if (obj.lenght == (0x0800)) 
+        printf("Next protocol header is IPv4\n");
+    if (obj.lenght == (0x0806)) 
+        printf("Next protocol header is ARP\n");
+    if (obj.lenght == (0x86DD)) 
+        printf("Next protocol header is IPv6\n");
+    if (obj.lenght == (0x22F0))
+        printf("Next protocol header is AVTP\n");
+    if (obj.lenght == (0x8100))
+        printf("Next protocol header is VLAN\n");
 }
 
 int main()
@@ -70,7 +87,8 @@ int main()
     Header H1 = {0x01005E000009,0xC20117230123,0x0800};
     Header H2 = {0xC001A3023215,0xD11725631020,0x0650};
     Header H3 = {0xFFFFFFFFFFFF,0xE52432350302,0x1100};
-    parseheader(H2);
-    parseLE(H2);
-    typecheck(H2);
+    parseheader(H1);
+    parseLE(H1);
+    typecheck(H1);
+    next_proto(H1);
 }
